cdca234c88769e35b62fa2ddeb669d3011933e88
selprince <55705084+selprince@users.noreply.github.com>
2020-05-13 18:17:09 -0400

    Add files via upload

---
+++ AssociateProfitSplitter.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.0;
+
+
+// lvl 1: equal split
+
+contract AssociateProfitSplitter {
+    
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;// @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        employee_one = _one;
+        employee_two = _two;
+        employee_three = _three;
+    }
+
+    function balance() public view returns(uint) {
+        
+       
+        
+    }
+
+    function deposit() public payable {
+        // @TODO: Split `msg.value` into three
+        uint amount = msg.value / 3; // Your code here!
+
+        // @TODO: Transfer the amount to each employee
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
+
+        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
+        msg.sender.transfer(msg.value % 3);
+    }
+
+    function() external payable {
+        // @TODO: Enforce that the `deposit` function is called in the fallback function!
+        deposit();
+    }
+}
